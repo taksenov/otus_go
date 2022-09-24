5efe0f71814863ff0c7c8d980fb935d01166858e
Timofey Aksenov <taksenov@gmail.com>
2022-09-24 15:44:25 +0500

    hw03_frequency_analysis: feat: add base level solution

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,99 @@
+// Package hw03frequencyanalysis -- Otus Go. HW03.
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"math"
+	"sort"
+	"strings"
+)
+
+const (
+	maximumWords int = 10
+)
+
+type wordData struct {
+	count int
+	word  string
+}
+
+// Top10 -- подсчет частоты появления слов в тексте.
+func Top10(s string) []string {
+	words := strings.Fields(s)
+	cache := map[string]wordData{}
+	warnCache := make(map[string]wordData, maximumWords)
+
+	for _, val := range words {
+		if checkExist(&cache, val) {
+			t := get(&cache, val)
+			t.count = t.count + 1
+			add(&cache, val, t)
+			handleWarnCache(&warnCache, val, t)
+		} else {
+			t := wordData{count: 1, word: val}
+			add(&cache, val, t)
+			handleWarnCache(&warnCache, val, t)
+		}
+	}
+
+	res := buildSortedWords(&warnCache)
+
+	return res
+}
+
+func checkExist(c *map[string]wordData, k string) bool {
+	_, res := (*c)[k]
+	return res
+}
+
+func add(c *map[string]wordData, k string, v wordData) {
+	(*c)[k] = v
+}
+
+func get(c *map[string]wordData, k string) wordData {
+	res := (*c)[k]
+	return res
+}
+
+func handleWarnCache(c *map[string]wordData, k string, v wordData) {
+	add(c, k, v)
+
+	if len((*c)) > maximumWords {
+		wordWithMinCount := wordData{count: int(math.Inf(1))}
+		for _, val := range *c {
+			if val.count < wordWithMinCount.count {
+				wordWithMinCount = wordData{count: val.count, word: val.word}
+			}
+		}
+		delete((*c), wordWithMinCount.word)
+	}
+}
+
+func buildSortedWords(c *map[string]wordData) []string {
+	if len((*c)) == 0 {
+		res := make([]string, 0)
+		return res
+	}
+
+	tS := make([]wordData, 0)
+
+	for _, val := range *c {
+		tS = append(tS, val)
+	}
+
+	sort.Slice(tS, func(i, j int) bool {
+		cond := tS[i].count == tS[j].count
+		if cond {
+			return tS[i].word < tS[j].word
+		}
+
+		return tS[i].count > tS[j].count
+	})
+
+	res := make([]string, int(math.Abs(float64(maximumWords-(maximumWords+len((*c)))))))
+
+	for idx, val := range tS {
+		res[idx] = val.word
+	}
+
+	return res
 }
