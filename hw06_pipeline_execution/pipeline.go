60ebe2405844f0ee0763ef7e6c4d5b1b4794b8e4
Timofey Aksenov <taksenov@gmail.com>
2022-10-30 17:27:52 +0500

    hw06_pipeline_execution: feat: HW06 is complete

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,45 @@
+// Package hw06pipelineexecution -- Otus Go HW06.
 package hw06pipelineexecution
 
 type (
-	In  = <-chan interface{}
+	// In job input.
+	In = <-chan interface{}
+	// Out job output.
 	Out = In
-	Bi  = chan interface{}
+	// Bi channel.
+	Bi = chan interface{}
 )
 
+// Stage -- job typing.
 type Stage func(in In) (out Out)
 
+// ExecutePipeline -- a few more code-lines and gitlab will go bankrupt.
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	for _, stage := range stages {
+		ch := make(Bi)
+		out = worker(done, ch, stage, out)
+	}
+
+	return out
+}
+
+func worker(done In, ch Bi, stage Stage, out Out) Out {
+	go func(ch Bi, out Out) {
+		defer close(ch)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-out:
+				if !ok {
+					return
+				}
+				ch <- val
+			}
+		}
+	}(ch, out)
+
+	return stage(ch)
 }
