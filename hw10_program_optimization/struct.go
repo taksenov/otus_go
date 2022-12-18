25995456ad077ac7262830fb6f0b3c04431854a4
Timofey Aksenov <taksenov@gmail.com>
2022-12-18 14:56:57 +0500

    hw10_program_optimization: feat: add easyjson codegeneration

---
+++ hw10_program_optimization/struct.go
@@ -0,0 +1,12 @@
+package hw10programoptimization
+
+//easyjson:json
+type User struct {
+	ID       int    `json:"Id"`
+	Name     string `json:"Name"`
+	Username string `json:"Username"`
+	Email    string `json:"Email"`
+	Phone    string `json:"Phone"`
+	Password string `json:"Password"`
+	Address  string `json:"Address"`
+}
