e0792b49e8fd7195e84a5b48143a7f3554bf5b0e
Timofey Aksenov <taksenov@gmail.com>
2022-12-18 15:11:22 +0500

    hw10_program_optimization: style: gofumpted

--- hw10_program_optimization/struct.go
+++ hw10_program_optimization/struct.go
@@ -2,11 +2,11 @@ package hw10programoptimization
 
 //easyjson:json
 type User struct {
-	ID       int    `json:"Id"` //nolint:tagliatelle
-	Name     string `json:"Name"` //nolint:tagliatelle
+	ID       int    `json:"Id"`       //nolint:tagliatelle
+	Name     string `json:"Name"`     //nolint:tagliatelle
 	Username string `json:"Username"` //nolint:tagliatelle
-	Email    string `json:"Email"` //nolint:tagliatelle
-	Phone    string `json:"Phone"` //nolint:tagliatelle
+	Email    string `json:"Email"`    //nolint:tagliatelle
+	Phone    string `json:"Phone"`    //nolint:tagliatelle
 	Password string `json:"Password"` //nolint:tagliatelle
-	Address  string `json:"Address"` //nolint:tagliatelle
+	Address  string `json:"Address"`  //nolint:tagliatelle
 }
