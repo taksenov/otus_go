860302f4681f899d0e6aee2e3f88f62fa6bf4084
Timofey Aksenov <taksenov@gmail.com>
2023-01-16 15:09:10 +0500

    hw11_telnet_client: feat: add business logic

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,10 +1,15 @@
+// Package hw11_telnet_client -- OTUS HW11 Telnet Client.
 package main
 
 import (
+	"bufio"
+	"errors"
 	"io"
+	"net"
 	"time"
 )
 
+// TelnetClient implementation.
 type TelnetClient interface {
 	Connect() error
 	io.Closer
@@ -12,10 +17,55 @@ type TelnetClient interface {
 	Receive() error
 }
 
+// NewTelnetClient constructor.
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	return &clientAbstraction{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+type clientAbstraction struct {
+	address  string
+	timeout  time.Duration
+	in       io.ReadCloser
+	out      io.Writer
+	conn     net.Conn
+	inScan   *bufio.Scanner
+	connScan *bufio.Scanner
+}
+
+func (t *clientAbstraction) Connect() (err error) {
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+	t.connScan = bufio.NewScanner(t.conn)
+	t.inScan = bufio.NewScanner(t.in)
+	return err
+}
+
+func (t *clientAbstraction) Close() error {
+	return t.conn.Close()
+}
+
+func (t *clientAbstraction) Send() (err error) {
+	if t.conn == nil {
+		return
+	}
+	if !t.inScan.Scan() {
+		return errors.New("end")
+	}
+	_, err = t.conn.Write(append(t.inScan.Bytes(), '\n'))
+	return
+}
+
+func (t *clientAbstraction) Receive() (err error) {
+	if t.conn == nil {
+		return
+	}
+	if !t.connScan.Scan() {
+		return errors.New("connection closed")
+	}
+	_, err = t.out.Write(append(t.connScan.Bytes(), '\n'))
+	return
+}
