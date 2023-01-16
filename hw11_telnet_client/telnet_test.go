7818905d770fe33c77f01af9adf6768e962fdfdc
Timofey Aksenov <taksenov@gmail.com>
2023-01-16 16:01:22 +0500

    hw11_telnet_client: test: add new unit-test cases

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
 	"net"
 	"sync"
@@ -62,4 +63,78 @@ func TestTelnetClient(t *testing.T) {
 
 		wg.Wait()
 	})
+}
+
+func testRun(t *testing.T, wg *sync.WaitGroup, l net.Listener) {
+	t.Helper()
+	go func() {
+		defer wg.Done()
+
+		in := &bytes.Buffer{}
+		out := &bytes.Buffer{}
+
+		timeout, err := time.ParseDuration("60s")
+		require.NoError(t, err)
+
+		client := NewTelnetClient(l.Addr().String(), timeout, io.NopCloser(in), out)
+		require.NoError(t, client.Connect())
+		defer func() { require.NoError(t, client.Close()) }()
+
+		in.WriteString("HELLO\n")
+		err = client.Send()
+		require.NoError(t, err)
+
+		err = client.Receive()
+		require.NoError(t, err)
+		require.Equal(t, "WORLD!\n", out.String())
+	}()
+}
+
+func testCheckConnect(t *testing.T, wg *sync.WaitGroup, l net.Listener) {
+	t.Helper()
+	defer wg.Done()
+
+	conn, err := l.Accept()
+	require.NoError(t, err)
+	require.NotNil(t, conn)
+	defer func() { require.NoError(t, conn.Close()) }()
+
+	request := make([]byte, 1024)
+	n, err := conn.Read(request)
+	require.NoError(t, err)
+	require.Equal(t, "HELLO\n", string(request)[:n])
+
+	n, err = conn.Write([]byte("WORLD!\n"))
+	require.NoError(t, err)
+	require.NotEqual(t, 0, n)
+}
+
+func testCloseServer(t *testing.T, wg *sync.WaitGroup, clientWg *sync.WaitGroup, l net.Listener) {
+	t.Helper()
+	defer func() {
+		wg.Done()
+		clientWg.Done()
+	}()
+
+	conn, err := l.Accept()
+	require.NoError(t, err)
+	require.NotNil(t, conn)
+	defer func() { require.NoError(t, conn.Close()) }()
+}
+
+func testCloseClient(t *testing.T, wg *sync.WaitGroup, clientWg *sync.WaitGroup, l net.Listener) {
+	t.Helper()
+	defer func() {
+		wg.Done()
+		clientWg.Done()
+	}()
+
+	conn, err := l.Accept()
+	require.NoError(t, err)
+	require.NotNil(t, conn)
+	defer func() { require.NoError(t, conn.Close()) }()
+
+	request := make([]byte, 1024)
+	_, err = conn.Read(request)
+	require.NoError(t, err)
 }
